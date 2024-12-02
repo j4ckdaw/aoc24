838939b5de3b9c3abe43bf2a9ae32e8351c8b06f
manu <j4ckdaw@protonmail.com>
2024-12-02 11:06:09 +0000

    typing

--- src/day2.py
+++ src/day2.py
@@ -2,7 +2,7 @@ from util import read_inputs
 from typing import List
 
 
-def is_safe(report: List[str]) -> bool:
+def is_safe(report: List[int]) -> bool:
     differences = [report[i + 1] - report[i] for i in range(0, len(report) - 1)]
 
     return all([d >= 1 and d <= 3 for d in differences]) or all(
@@ -19,7 +19,7 @@ def part_a(input):
 def part_b(input):
     reports = [[int(n) for n in line.split()] for line in input]
 
-    def is_safe_with_dampener(r: List[str]) -> bool:
+    def is_safe_with_dampener(r: List[int]) -> bool:
         if is_safe(r):
             return True
 
